131082c1d01f9259b5622f5b43923caa1d17e735
Alex Chan <chana98@uw.edu>
2021-11-01 22:32:51 -0700

    created sample navigation bar and homepage

---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Navigation from '../Components/Navigation';
+
+export default function Home() {
+    return (
+        <div>
+            <Navigation />
+            <p>Test</p>
+        </div>
+    )
+}
