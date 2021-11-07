ddb3fac27f0fccc5e2afc8a4a8d8cb41b2f401d7
Alex Chan <chana98@uw.edu>
2021-11-06 21:28:22 -0700

    added two more pages to work on

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from './Pages/Home';
+import CalculatorPage from './Pages/CalculatorPage';
+import KitchenPage from './Pages/KitchenPage';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Router>
           <Switch>
             <Route path="/" exact component={() => <Home />} />
+            <Route path="/calculator" exact component={() => <CalculatorPage />} />
+            <Route path="/kitchen" exact component={() => <KitchenPage />} />
           </Switch>
         </Router>
       </header>
---
+++ src/Pages/CalculatorPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navigation from '../Components/Navigation';
+
+export default function CalculatorPage() {
+    return (
+        <div>
+            <Navigation />
+            <p>Test for calculator page</p>
+        </div>
+    )
+}
