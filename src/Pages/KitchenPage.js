a81cb2f7301cbcf23722e0575f9c99656f954987
maif2338 <59621871+maif2338@users.noreply.github.com>
2021-11-18 09:12:14 -0800

    changed buttons

--- src/Pages/KitchenPage.js
+++ src/Pages/KitchenPage.js
@@ -2,37 +2,40 @@ import React from 'react';
 import Navigation from '../Components/Navigation';
 
 export default function KitchenPage() {
-  var modal = document.getElementById("myModal");
     return (
-        <div>
-            <Navigation />
-            <p>Test for Kitchen Page</p>
-            <button id="myBtn">Hand-washing</button>
-            <div id="myModal" class="modal">
-            <div class="modal-content">
-           <span class="close">&times;</span>
-    <p>Hand-washing dishes is the best practice to conserve water! 
-      The best method for Hand-washing dishes is the 2 basin method!
-    </p>
-  </div>
-
-</div>
-    
-    <button id="mydishBtn">Dish</button>
-
-   
-    <div id="myModal" class="modal">
-    
-     
-      <div class="modal-content">
-        <span class="close">&times;</span>
-        <p>Some text in the Modal..</p>
+      <div>
+        <Navigation />
+        <p>Test for Kitchen Page</p>
+           <button id="myBtn" onClick={ () => {
+            let modal = document.getElementById("myModal");
+            console.log(modal);
+            modal.style.display = "block";
+          }}>Hand-washing</button>
+        <div id="myModal" class="modal">
+          <div class="modal-content">
+            <span class="close">&times;</span>
+            <p>Hand-washing dishes is the best practice to conserve water! 
+            The best method for Hand-washing dishes is the 2 basin method!</p>
+          </div>
+        </div>
+        <button id="mydishBtn" onClick={()=>{
+          let modal = document.getElementById("mydishModal");
+          console.log(modal);
+          modal.style.display = "block";
+          // code goes here
+        }}>Dish</button>
+        <div id="mydishModal" class="modal">
+          <div class="modal-content">
+          <span class="close">&times;</span>
+          <p>Dishwashing uses more water, but these a great
+             dishwashers that are more efficent!</p>
+          </div>
+        </div> 
+       
       </div>
-        </div>
-        </div>
     )
    
-}
+
 
 // Get the modal
 //var modal = document.getElementById("myModal");
@@ -53,19 +56,20 @@ btn.onclick = function() {
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+  var modal = document.getElementById("myModal")
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
- 
+  var modal = document.getElementById("myModal")
   if (event.target == modal) {
     console.log(modal)
     modal.style.display = "none";
   }
 }
 // Get the modal
-var modal = document.getElementById("mydishModal");
+//var modal = document.getElementById("mydishModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("mydishBtn");
@@ -76,16 +80,20 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
+  var modal = document.getElementById("mydishModal");
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
+  var modal = document.getElementById("mydishModal");
   modal.style.display = "none";
 }
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
+  var modal = document.getElementById("mydishModal");
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
 }
