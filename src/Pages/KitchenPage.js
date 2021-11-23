d236498a6ba577e6cfadf02442688b0fbeb5453a
maif2338 <59621871+maif2338@users.noreply.github.com>
2021-11-23 09:36:47 -0800

    finished buttons

--- src/Pages/KitchenPage.js
+++ src/Pages/KitchenPage.js
@@ -6,27 +6,36 @@ export default function KitchenPage() {
       <div>
         <Navigation />
         <p>Test for Kitchen Page</p>
-           <button id="myBtn" onClick={ () => {
+           <button id="myBtn" onClick={()=> {
             let modal = document.getElementById("myModal");
-            console.log(modal);
             modal.style.display = "block";
           }}>Hand-washing</button>
+
         <div id="myModal" class="modal">
           <div class="modal-content">
-            <span class="close">&times;</span>
+         
+          
+          <span class="close" onClick= {() => {
+              let modal = document.getElementById("myModal");
+              modal.style.display = "none";
+          }}>&times;</span>
+          
+
             <p>Hand-washing dishes is the best practice to conserve water! 
             The best method for Hand-washing dishes is the 2 basin method!</p>
           </div>
         </div>
         <button id="mydishBtn" onClick={()=>{
           let modal = document.getElementById("mydishModal");
-          console.log(modal);
           modal.style.display = "block";
-          // code goes here
         }}>Dish</button>
         <div id="mydishModal" class="modal">
           <div class="modal-content">
-          <span class="close">&times;</span>
+          
+          <span class="close" onClick= {() => {
+              let modal = document.getElementById("mydishModal");
+              modal.style.display = "none";
+          }}>&times;</span>
           <p>Dishwashing uses more water, but these a great
              dishwashers that are more efficent!</p>
           </div>
